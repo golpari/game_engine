b9171856749f2f131db8d30883deb0c11e28b375
golpari <golpari@umich.edu>
2024-01-25 10:43:38 -0500

    test suite 0

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
-#include "glm/glm.hpp"
+#include "MapHelper.h"
 
 int main() {
 
-    std::cout << "Hello, World!" << std::endl;
+    // on launch, print game_start_message
+    std::cout << game_start_message << std::endl;
+
+    // print initial render
+    std::cout << initial_render << std::endl;
+
+    int health = 3;
+    int score = 0;
+    // print player health and score
+    std::cout << "health : " << health << std::endl
+        << "score : " << score << std::endl;
+
+    //prompt user
+    std::cout << "Please make a decision..." << std::endl;
+
+    //explain user options
+    std::cout << "Your options are \"n\", \"e\", \"s\", \"w\", \"quit\"" << std::endl;
+
+    std::string userInput;
+    while (std::cin >> userInput && userInput != "quit") {
+        // print initial render
+        std::cout << initial_render << std::endl;
+
+        int health = 3;
+        int score = 0;
+        // print player health and score
+        std::cout << "health : " << health << std::endl
+            << "score : " << score << std::endl;
+
+        //prompt user
+        std::cout << "Please make a decision..." << std::endl;
+
+        //explain user options
+        std::cout << "Your options are \"n\", \"e\", \"s\", \"w\", \"quit\"" << std::endl;
+    }
+
+    std::cout << game_over_bad_message;
   
     return 0; 
 }
