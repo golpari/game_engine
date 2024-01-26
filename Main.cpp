d6dc2e23fac6cb653b67abe2cc0878e82b12cf87
golpari <golpari@umich.edu>
2024-01-26 17:48:30 -0500

    switched to do while loop

--- Main.cpp
+++ Main.cpp
@@ -8,23 +8,9 @@ int main() {
     // on launch, print game_start_message
     std::cout << game_start_message << std::endl;
 
-    // print initial render
-    std::cout << initial_render << std::endl;
-
-    int health = 3;
-    int score = 0;
-    // print player health and score
-    std::cout << "health : " << health << ", "
-        << "score : " << score << std::endl;
-
-    //prompt user
-    std::cout << "Please make a decision..." << std::endl;
-
-    //explain user options
-    std::cout << "Your options are \"n\", \"e\", \"s\", \"w\", \"quit\"" << std::endl;
-
     std::string userInput;
-    while (std::cin >> userInput && userInput != "quit") {
+    do {
+        // print map
         // print initial render
         std::cout << initial_render << std::endl;
 
@@ -39,9 +25,20 @@ int main() {
 
         //explain user options
         std::cout << "Your options are \"n\", \"e\", \"s\", \"w\", \"quit\"" << std::endl;
-    }
+    } while (std::cin >> userInput && userInput != "quit");
 
     std::cout << game_over_bad_message;
   
     return 0; 
+}
+
+// given some movement command, update the relevant actor
+void UpdateActorPosition(Actor actor, char movement) {
+    return;
+}
+
+
+void PrintCameraView(glm::ivec2 playerPosition) {
+
+    return;
 }
