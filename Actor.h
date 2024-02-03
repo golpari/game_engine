2de09252366441c675bb0168c455f64a9e1b802b
golpari <golpari@umich.edu>
2024-02-03 14:49:16 -0500

    actor constructor optimizations please grade homework4 windows

--- Actor.h
+++ Actor.h
@@ -19,7 +19,18 @@ public:
 	bool scoredUpped;
 
 	Actor(std::string actor_name_in, char view_in, glm::ivec2 position_in, glm::ivec2 initial_velocity_in,
-		bool blocking_in, std::string nearby_dialogue_in, std::string contact_dialogue_in);
+		bool blocking_in, std::string nearby_dialogue_in, std::string contact_dialogue_in)
+		: actor_name(std::move(actor_name_in)), // Use std::move for strings
+		actorID(0), // Directly initialize to 0
+		position(std::move(position_in)), // Use std::move if glm::ivec2 has a move constructor; otherwise, it's fine as is
+		velocity(std::move(initial_velocity_in)), // Same as position
+		view(view_in),
+		blocking(blocking_in),
+		nearby_dialogue(std::move(nearby_dialogue_in)), // Use std::move for strings
+		contact_dialogue(std::move(contact_dialogue_in)), // Use std::move for strings
+		scoredUpped(false) // Directly initialize to false
+	{}
+
 
 	Actor();
 };
